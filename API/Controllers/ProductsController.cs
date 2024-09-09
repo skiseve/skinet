6dbc559c4983d23c9462bd6b33f742be55329488
skiseve <skiseve@gmail.com>
2024-09-09 17:27:01 +0300

    Initial

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.controllers
+{
+    //[ApiController, Route("")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id) 
+        {
+            var product = await _context.Products.FindAsync(id);//.Products.SingleOrDefault();
+            //var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            return Ok(product);
+            //return await _context.Products.FindAsync(id);
+        }
+        
+    }
+}
