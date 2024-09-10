0e641fe8151cb2deebd7628efb0d17603989d738
skiseve <skiseve@gmail.com>
2024-09-10 05:56:05 +0300

    End of Section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,26 +11,42 @@ namespace API.controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        private readonly IProductRepo _repo;
+
+        public ProductsController(IProductRepo repo)
         {
-            _context = context;
+            _repo = repo;            
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            // var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetAllAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id) 
         {
-            var product = await _context.Products.FindAsync(id);//.Products.SingleOrDefault();
-            //var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            // var product = _context.Products.FirstOrDefault(x => x.Id == id);
+            // var product = await _context.Products.FindAsync(id);//.Products.SingleOrDefault();
+            var product = await _repo.GetByIdAsync(id);
             return Ok(product);
-            //return await _context.Products.FindAsync(id);
+            // return await _context.Products.FindAsync(id);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            // return await _repo.GetProductBrandsAsync();
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypess()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
         }
         
     }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,12 +9,33 @@ builder.Services.AddControllers(); //
 builder.Services.AddDbContext<StoreContext>(x => 
     x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IProductRepo, ProductRepo>();
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+#region Automate Db creation and Migrations Running and Seed Data
+using (var scope = app.Services.CreateScope()) 
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error occured during migration");
+    }
+}
+#endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
